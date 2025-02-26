9e779e338aa93cf33231831530ecde66bac91332
phoenixkea <148383432+phoenixkea@users.noreply.github.com>
2025-02-26 17:32:14 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+## cci website workshop template
+
+Template for the cci website making [workshop](https://wiki.cci.arts.ac.uk/books/how-to-guides/page/making-websites-and-putting-them-online), made using jQuery. To run the website, open the html file in your browser :)
---
+++ main.js
@@ -0,0 +1,15 @@
+function changeBackground() {
+	console.log('changing')
+	$('body').css({'backgroundColor': 'yellow'});
+}
+
+
+function rotate() {
+	console.log('rotating')
+	$('body').css({'transform': 'rotate(10deg)'});
+}
+
+function rotate() {
+	console.log('rotating')
+	$('body').css({'transform': 'rotate(60deg)'});
+}
